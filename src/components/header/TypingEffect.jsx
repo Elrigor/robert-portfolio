d4551cadef21a3ac5a3f14e7908ba82d50b79f02
Robert <robert.maracine25@gmail.com>
2023-06-09 17:26:36 +0200

    optimized typing effect

--- src/components/header/TypingEffect.jsx
+++ src/components/header/TypingEffect.jsx
@@ -4,62 +4,47 @@ import "./header.css";
 
 const TypingEffect = () => {
   const { t } = useTranslation();
-  
+
   const sentences = useMemo(() => [
     "Robert Mihai Maracine",
     t("developer"),
   ], [t]);
 
-  const [currentSentence, setCurrentSentence] = useState(0);
   const [currentText, setCurrentText] = useState("");
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
-    let timeout;
-
-    if (!isDeleting && currentIndex === sentences[currentSentence]?.length) {
-      timeout = setTimeout(() => {
-        setIsDeleting(true);
-      }, 1000);
-    } else if (isDeleting && currentIndex === 0) {
-      timeout = setTimeout(() => {
-        setIsDeleting(false);
-        setCurrentSentence((prevSentence) =>
-          prevSentence === sentences.length - 1 ? 0 : prevSentence + 1
-        );
-      }, 500);
-    }
-
-    return () => clearTimeout(timeout);
-  }, [currentSentence, currentIndex, isDeleting, sentences]);
-
-  useEffect(() => {
-    if (
-      currentSentence >= sentences.length - 1 &&
-      isDeleting &&
-      currentIndex === 0
-    ) {
-      return;
-    }
-
-    const interval = setInterval(() => {
-      if (
-        !isDeleting &&
-        sentences[currentSentence]?.[currentIndex] !== undefined
-      ) {
-        setCurrentText(
-          (prevText) => prevText + sentences[currentSentence][currentIndex]
-        );
-        setCurrentIndex((prevIndex) => prevIndex + 1);
-      } else if (isDeleting && currentIndex > 0) {
-        setCurrentText((prevText) => prevText.slice(0, prevText.length - 1));
-        setCurrentIndex((prevIndex) => prevIndex - 1);
+    let isMounted = true;
+    let currentSentence = 0;
+    const typeSentence = async (sentence, deleteAfter = true) => {
+      for (let i = 0; i < sentence.length; i++) {
+        if (!isMounted) return;
+        setCurrentText(sentence.slice(0, i+1));
+        await new Promise(resolve => setTimeout(resolve, 65));
       }
-    }, 65);
+      
+      if (deleteAfter) {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        for (let i = sentence.length; i >= 0; i--) {
+          if (!isMounted) return;
+          setCurrentText(sentence.slice(0, i));
+          await new Promise(resolve => setTimeout(resolve, 65));
+        }
+      }
+    };
 
-    return () => clearInterval(interval);
-  }, [currentSentence, currentIndex, isDeleting, sentences]);
+    const typeAllSentences = async () => {
+      while (isMounted) {
+        await typeSentence(sentences[currentSentence]);
+        currentSentence = (currentSentence + 1) % sentences.length;
+      }
+    };
+
+    typeAllSentences();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [sentences]);
 
   const isTextEmpty = currentText.length === 0;
 
