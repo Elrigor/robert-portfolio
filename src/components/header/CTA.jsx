4f71feeae74319584803fb893cde1f0d4bd24b8f
Elrigor <80095124+Elrigor@users.noreply.github.com>
2024-12-24 00:16:55 +0100

    CV update on code

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import CV from "../../assets/cv.pdf";
+import CV from "../../assets/personal_cv.pdf";
 import { useTranslation } from "react-i18next";
 
 const CTA = () => {
