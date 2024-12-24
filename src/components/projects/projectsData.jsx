c484389467ba12c282563df3ce5b019937afcff4
Elrigor <80095124+Elrigor@users.noreply.github.com>
2024-12-24 22:53:35 +0100

    add new projects

--- src/components/projects/projectsData.jsx
+++ src/components/projects/projectsData.jsx
@@ -1,20 +1,25 @@
 import hutao from "../../assets/hutao.png";
+import roxy from "../../assets/roxy.png";
 import wizardeo from "../../assets/wizardeo.png";
 import mihaim from "../../assets/mihaim.png";
 import portfolio from "../../assets/portfolio.png";
 import tools from "../../assets/tools.png";
-import task from "../../assets/task.png"; 
+// import task from "../../assets/task.png"; 
 import hutaobot from "../../assets/hutaobot.png";
 import ayamiweb from "../../assets/ayamiweb.png";
+import ayami from "../../assets/ayami.png";
+import k1po4 from "../../assets/k1po4.png";
+import dashboard from "../../assets/dashboard.png";
+import hao from "../../assets/hao.png";
 
 
 const data = [
   {
-    image: hutao,
-    title: "hutaoweb",
-    github: "https://github.com/Elrigor/hutaoWeb",
-    demo: "https://elrigor.github.io/HuTaoWebDemo/index.html",
-    label:"Django, Python, HTML, CSS, JS, Bootstrap, SQLite"
+    image: portfolio,
+    title: "portfolio",
+    github: "https://github.com/Elrigor/robert-portfolio",
+    demo: "https://robert.soy",
+    label:"ReactJS, EmailJS, HTML, CSS, JS, i18n"
   },
   {
     image: wizardeo,
@@ -24,33 +29,39 @@ const data = [
     label:"Wagtail, Django, Python, HTML, CSS, JS, Bootstrap, PostgreSQL, JQuery"
   }, 
   {
-    image: mihaim,
-    title: "mihaim",
-    github: "",
-    demo: "https://mihaim.es",
-    label:"WordPress, HTML, CSS, JS"
-  },   
-
-  {
-    image: portfolio,
-    title: "portfolio",
-    github: "https://github.com/Elrigor/robert-portfolio",
-    demo: "https://robert.soy",
-    label:"ReactJS, HTML, CSS, JS, i18n"
-  },
-  {
     image: tools,
     title: "tools",
     github: "",
     demo: "https://tools.wizardeo.es",
-    label:"VueJS, Vuetify, NuxtJS, HTML, CSS, JS"
+    label:"VueJS, Vuetify, NuxtJS, i18n, HTML, CSS, JS"
   },
   {
-    image: task,
-    title: "task",
-    github: "https://github.com/Elrigor/React-Tasks-Application",
-    demo: "https://elrigor.github.io/React-Tasks-Application/",
-    label:"Tailwind, ReactJS, HTML, CSS, JS"
+    image: ayamiweb,
+    title: "ayamiweb",
+    github: "",
+    demo: "https://ayami2059.wixsite.com/ayami2059",
+    label:"Wix, HTML, CSS, JS"
+  },
+  {
+    image: ayami,
+    title: "ayami",
+    github: "",
+    demo: "https://ayami2059.stream/",
+    label:"TypeScript, Tailwind, ReactJS, NextJS, EmailJS, HTML, CSS"
+  },
+  {
+    image: k1po4,
+    title: "k1po4",
+    github: "",
+    demo: "https://k1po4.pages.dev",
+    label:"TypeScript, Tailwind, ReactJS, NextJS, HTML, CSS"
+  },
+  {
+    image: roxy,
+    title: "roxy",
+    github: "",
+    demo: "https://elrigor.github.io/roxy-migurdia-web/",
+    label:"TypeScript, Tailwind, ReactJS, NextJS, HTML, CSS"
   },
   {
     image: hutaobot,
@@ -60,12 +71,41 @@ const data = [
     label:"DiscordPY, Python, SQLite"
   }, 
   {
-    image: ayamiweb,
-    title: "ayamiweb",
+    image: hutao,
+    title: "hutaoweb",
+    github: "https://github.com/Elrigor/hutaoWeb",
+    demo: "https://elrigor.github.io/HuTaoWebDemo/index.html",
+    label:"Django, Python, HTML, CSS, JS, Bootstrap, SQLite"
+  },
+  {
+    image: hao,
+    title: "hao",
     github: "",
-    demo: "https://ayami2059.wixsite.com/ayami2059",
-    label:"Wix, HTML, CSS, JS"
-  }
+    demo: "https://discord.com/application-directory/1104497583084277882",
+    label:"DiscordJS, TypeScript, JS, Supabase, i18n"
+  }, 
+  {
+    image: dashboard,
+    title: "dashboard",
+    github: "",
+    demo: "",
+    label:"TypeScript, Tailwind, ReactJS, Supabase,NextJS, HTML, CSS"
+  },
+  {
+    image: mihaim,
+    title: "mihaim",
+    github: "",
+    demo: "",
+    label:"WordPress, HTML, CSS, JS"
+  },   
+  // {
+  //   image: task,
+  //   title: "task",
+  //   github: "https://github.com/Elrigor/React-Tasks-Application",
+  //   demo: "https://elrigor.github.io/React-Tasks-Application/",
+  //   label:"Tailwind, ReactJS, HTML, CSS, JS"
+  // },
+
 ];
 
 export default data;
